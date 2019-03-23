02814b7126c8341da0d4e71a52690459ce97a539
Tomáš Novotný <tomas.novotny@inspirum.cz>
2019-03-23 10:14:09 +0100

    Update to support Balikobot API v1.851

---
+++ src/Definitions/Region.php
@@ -0,0 +1,692 @@
+<?php
+
+namespace Inspirum\Balikobot\Definitions;
+
+class Region
+{
+    /**
+     * Alabama
+     *
+     * @var string
+     */
+    public const US_ALABAMA = 'AL';
+    
+    /**
+     * Alaska
+     *
+     * @var string
+     */
+    public const US_ALASKA = 'AK';
+    
+    /**
+     * American Samoa
+     *
+     * @var string
+     */
+    public const US_AMERICAN_SAMOA = 'AS';
+    
+    /**
+     * Arizona
+     *
+     * @var string
+     */
+    public const US_ARIZONA = 'AZ';
+    
+    /**
+     * Arkansas
+     *
+     * @var string
+     */
+    public const US_ARKANSAS = 'AR';
+    
+    /**
+     * California
+     *
+     * @var string
+     */
+    public const CALIFORNIA = 'CA';
+    
+    /**
+     * Colorado
+     *
+     * @var string
+     */
+    public const US_COLORADO = 'CO';
+    
+    /**
+     * Connecticut
+     *
+     * @var string
+     */
+    public const US_CONNECTICUT = 'CT';
+    
+    /**
+     * Delaware
+     *
+     * @var string
+     */
+    public const US_DELAWARE = 'DE';
+    
+    /**
+     * District of Columbia
+     *
+     * @var string
+     */
+    public const US_DISCTRICT_OF_COLUMBIA = 'DC';
+    
+    /**
+     * Federated States of Micronesia
+     *
+     * @var string
+     */
+    public const US_FEDERATED_STATES_OF_MICRONESIA = 'FM';
+    
+    /**
+     * Florida
+     *
+     * @var string
+     */
+    public const US_FLORIDA = 'FL';
+    
+    /**
+     * Georgia
+     *
+     * @var string
+     */
+    public const US_GEORGIA = 'GA';
+    
+    /**
+     * Guam
+     *
+     * @var string
+     */
+    public const US_GUAM = 'GU';
+    
+    /**
+     * Hawaii
+     *
+     * @var string
+     */
+    public const US_HAWAI = 'HI';
+    
+    /**
+     * Idaho
+     *
+     * @var string
+     */
+    public const US_IDAHO = 'ID';
+    
+    /**
+     * Illinois
+     *
+     * @var string
+     */
+    public const US_ILLINOIS = 'IL';
+    
+    /**
+     * Indiana
+     *
+     * @var string
+     */
+    public const US_INDIANA = 'IN';
+    
+    /**
+     * Iowa
+     *
+     * @var string
+     */
+    public const US_IOWA = 'IA';
+    
+    /**
+     * Kansas
+     *
+     * @var string
+     */
+    public const US_KANSAS = 'KS';
+    
+    /**
+     * Kentucky
+     *
+     * @var string
+     */
+    public const US_KENTUCKY = 'KY';
+    
+    /**
+     * Louisiana
+     *
+     * @var string
+     */
+    public const US_LOUISIANA = 'LA';
+    
+    /**
+     * Maine
+     *
+     * @var string
+     */
+    public const US_MAINE = 'ME';
+    
+    /**
+     * Marshall Islands
+     *
+     * @var string
+     */
+    public const US_MARSHALL_ISLANDS = 'MH';
+    
+    /**
+     * Maryland
+     *
+     * @var string
+     */
+    public const US_MARYLAND = 'MD';
+    
+    /**
+     * Massachusetts
+     *
+     * @var string
+     */
+    public const US_MASSACHUETTS = 'MA';
+    
+    /**
+     * Michigan
+     *
+     * @var string
+     */
+    public const US_MICHIGAN = 'MI';
+    
+    /**
+     * Minnesota
+     *
+     * @var string
+     */
+    public const US_MINNESOTA = 'MN';
+    
+    /**
+     * Mississippi
+     *
+     * @var string
+     */
+    public const US_MISSISSIPPI = 'MS';
+    
+    /**
+     * Missouri
+     *
+     * @var string
+     */
+    public const US_MISSOURI = 'MO';
+    
+    /**
+     * Montana
+     *
+     * @var string
+     */
+    public const US_MONTANA = 'MT';
+    
+    /**
+     * Nebraska
+     *
+     * @var string
+     */
+    public const US_NEBRASKA = 'NE';
+    
+    /**
+     * Nevada
+     *
+     * @var string
+     */
+    public const US_NEVADA = 'NV';
+    
+    /**
+     * New Hampshire
+     *
+     * @var string
+     */
+    public const US_NEW_HAMPSHIRE = 'NH';
+    
+    /**
+     * New Jersey
+     *
+     * @var string
+     */
+    public const US_NEW_JERSEY = 'NJ';
+    
+    /**
+     * New Mexico
+     *
+     * @var string
+     */
+    public const US_NEW_MEXICO = 'NM';
+    
+    /**
+     * New York
+     *
+     * @var string
+     */
+    public const US_NEW_YORK = 'NY';
+    
+    /**
+     * North Carolina
+     *
+     * @var string
+     */
+    public const US_NORTH_CAROLINA = 'NC';
+    
+    /**
+     * North Dakota
+     *
+     * @var string
+     */
+    public const US_NORTH_DAKOTA = 'ND';
+    
+    /**
+     * Northern Mariana Islands
+     *
+     * @var string
+     */
+    public const US_NORTHEN_MARIANA_ISLANDS = 'MP';
+    
+    /**
+     * Ohio
+     *
+     * @var string
+     */
+    public const US_OHIO = 'OH';
+    
+    /**
+     * Oklahoma
+     *
+     * @var string
+     */
+    public const US_OKLAHOMA = 'OK';
+    
+    /**
+     * Oregon
+     *
+     * @var string
+     */
+    public const US_OREGON = 'OR';
+    
+    /**
+     * Palau
+     *
+     * @var string
+     */
+    public const US_PALAU = 'PW';
+    
+    /**
+     * Pennsylvania
+     *
+     * @var string
+     */
+    public const US_PENNSYLVANIA = 'PA';
+    
+    /**
+     * Puerto Rico
+     *
+     * @var string
+     */
+    public const US_PUERTO_RICO = 'PR';
+    
+    /**
+     * Rhode Island
+     *
+     * @var string
+     */
+    public const US_RHODE_ISLAND = 'RI';
+    
+    /**
+     * South Carolina
+     *
+     * @var string
+     */
+    public const US_SOUTH_CAROLINE = 'SC';
+    
+    /**
+     * South Dakota
+     *
+     * @var string
+     */
+    public const US_SOUTH_DAKOTA = 'SD';
+    
+    /**
+     * Tennessee
+     *
+     * @var string
+     */
+    public const US_TENNESSEE = 'TN';
+    
+    /**
+     * Texas
+     *
+     * @var string
+     */
+    public const US_TEXAS = 'TX';
+    
+    /**
+     * Utah
+     *
+     * @var string
+     */
+    public const US_UTAH = 'UT';
+    
+    /**
+     * Vermont
+     *
+     * @var string
+     */
+    public const US_VERMONT = 'VT';
+    
+    /**
+     * Virgin Islands
+     *
+     * @var string
+     */
+    public const US_VIRGIN_ISLANDS = 'VI';
+    
+    /**
+     * Virginia
+     *
+     * @var string
+     */
+    public const US_VIRGINIA = 'VA';
+    
+    /**
+     * Washington
+     *
+     * @var string
+     */
+    public const US_WASHINGTON = 'WA';
+    
+    /**
+     * West Virginia
+     *
+     * @var string
+     */
+    public const US_WEST_VIRGINIA = 'WV';
+    
+    /**
+     * Wisconsin
+     *
+     * @var string
+     */
+    public const US_WISCONSIN = 'WI';
+    
+    /**
+     * Wyoming
+     *
+     * @var string
+     */
+    public const US_WYOMING = 'WY';
+    
+    /**
+     * Alberta
+     *
+     * @var string
+     */
+    public const CA_ALBERTA = 'AB';
+    
+    /**
+     * British Columbia
+     *
+     * @var string
+     */
+    public const CA_BRITISH_COLUMBIA = 'BC';
+    
+    /**
+     * Manitoba
+     *
+     * @var string
+     */
+    public const CA_MANITOBA = 'MB';
+    
+    /**
+     * New Brunswick
+     *
+     * @var string
+     */
+    public const CA_NEW_BRUNSWICK = 'NB';
+    
+    /**
+     * Newfoundland and Labrador
+     *
+     * @var string
+     */
+    public const CA_NEWFOUNDLAND_AND_LABRADOR = 'NL';
+    
+    /**
+     * Northwest Territories
+     *
+     * @var string
+     */
+    public const CA_NORTHWEST_TERRITORIES = 'NT';
+    
+    /**
+     * Nova Scotia
+     *
+     * @var string
+     */
+    public const CA_NOVA_SCOTIA = 'NS';
+    
+    /**
+     * Nunavut
+     *
+     * @var string
+     */
+    public const CA_NUNAVUT = 'NU';
+    
+    /**
+     * Ontario
+     *
+     * @var string
+     */
+    public const CA_ONTARIO = 'ON';
+    
+    /**
+     * Prince Edward Island
+     *
+     * @var string
+     */
+    public const CA_PRINCE_EDWARD_ISLAND = 'PE';
+    
+    /**
+     * Quebec
+     *
+     * @var string
+     */
+    public const CA_QUEBEC = 'QC';
+    
+    /**
+     * Saskatchewan
+     *
+     * @var string
+     */
+    public const CA_SASKATCHEWAN = 'SK';
+    
+    /**
+     * Yukon
+     *
+     * @var string
+     */
+    public const CA_YUKON = 'YT';
+    
+    /**
+     * Carlow
+     *
+     * @var string
+     */
+    public const IE_CARLOW = 'CW';
+    
+    /**
+     * Cavan
+     *
+     * @var string
+     */
+    public const IE_CAVAN = 'CN';
+    
+    /**
+     * Clare
+     *
+     * @var string
+     */
+    public const IE_CLARE = 'CE';
+    
+    /**
+     * Cork
+     *
+     * @var string
+     */
+    public const IE_CORK = 'CO';
+    
+    /**
+     * Donegal
+     *
+     * @var string
+     */
+    public const IE_DONEGAL = 'DL';
+    
+    /**
+     * Dublin
+     *
+     * @var string
+     */
+    public const IE_DUBLIN = 'D';
+    
+    /**
+     * Galway
+     *
+     * @var string
+     */
+    public const IE_GALWAY = 'G';
+    
+    /**
+     * Kerry
+     *
+     * @var string
+     */
+    public const IE_KERRY = 'KY';
+    
+    /**
+     * Kildare
+     *
+     * @var string
+     */
+    public const IE_KILDARE = 'KE';
+    
+    /**
+     * Kilkenny
+     *
+     * @var string
+     */
+    public const IE_KILKENNY = 'KK';
+    
+    /**
+     * Laois
+     *
+     * @var string
+     */
+    public const IE_LAOIS = 'LS';
+    
+    /**
+     * Leitrim
+     *
+     * @var string
+     */
+    public const IE_LEITRIM = 'LM';
+    
+    /**
+     * Limerick
+     *
+     * @var string
+     */
+    public const IE_LIMERICK = 'LK';
+    
+    /**
+     * Longford
+     *
+     * @var string
+     */
+    public const IE_LONGFORD = 'LD';
+    
+    /**
+     * Louth
+     *
+     * @var string
+     */
+    public const IE_LOUTH = 'LH';
+    
+    /**
+     * Mayo
+     *
+     * @var string
+     */
+    public const IE_MAYO = 'MO';
+    
+    /**
+     * Meath
+     *
+     * @var string
+     */
+    public const IE_MEATH = 'MH';
+    
+    /**
+     * Monaghan
+     *
+     * @var string
+     */
+    public const IE_MONAGHAN = 'MN';
+    
+    /**
+     * Offaly
+     *
+     * @var string
+     */
+    public const IE_OFFALY = 'OY';
+    
+    /**
+     * Roscommon
+     *
+     * @var string
+     */
+    public const IE_ROSCOMMON = 'RN';
+    
+    /**
+     * Sligo
+     *
+     * @var string
+     */
+    public const IE_SLIGO = 'SO';
+    
+    /**
+     * Tipperary
+     *
+     * @var string
+     */
+    public const IE_TIPPERARY = 'TA';
+    
+    /**
+     * Waterford
+     *
+     * @var string
+     */
+    public const IE_WATERFORD = 'WD';
+    
+    /**
+     * Westmeath
+     *
+     * @var string
+     */
+    public const IE_WESTMEATH = 'WH';
+    
+    /**
+     * Wexford
+     *
+     * @var string
+     */
+    public const IE_WEXFORD = 'WX';
+    
+    /**
+     * Wicklow
+     *
+     * @var string
+     */
+    public const IE_WICKLOW = 'WW';
+}
