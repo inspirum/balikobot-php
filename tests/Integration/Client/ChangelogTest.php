4b06e0d551c9cad0c3dc0f95afd9a7307277fa80
Tomáš Novotný <tomas.novotny@inspirum.cz>
2021-02-02 08:25:31 +0100

    Add test for latest changes

---
+++ tests/Integration/Client/ChangelogTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Inspirum\Balikobot\Tests\Integration\Client;
+
+class ChangelogTest extends AbstractClientTestCase
+{
+    public function testLatestChangesSupport()
+    {
+        $service = $this->newClient();
+
+        $changelog = $service->getChangelog();
+
+        $expected = '1.901';
+        $actual   = $changelog['version'];
+
+        if ($actual !== $expected) {
+            $this->addWarning(sprintf('Package not supporting latest changes [%s > %s]', $actual, $expected));
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+}
